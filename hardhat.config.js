173b654ba7b0c634c4f04c09fc882df278e3baea
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-08-09 13:48:32 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.4",
+  mocha: {
+    reporter:"json"
+   },
+  networks:{
+     hardhat : {
+
+     }
+  }
 };
